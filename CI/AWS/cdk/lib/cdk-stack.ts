214e2f552edc03af109794ece31b99fb244b7348
sw-joni-l <joni.laari@sitowise.com>
2025-06-25 14:40:24 +0300

    passing stack build

--- CI/AWS/cdk/lib/cdk-stack.ts
+++ CI/AWS/cdk/lib/cdk-stack.ts
@@ -51,19 +51,13 @@ export class TestAutomationCodeBuildStack extends cdk.Stack {
           },
           build: {
             commands: [
-          //"docker pull ${DOCKER_ASSET_IMAGE_URI}",
-          //'docker run --rm public.ecr.aws/docker/library/alpine:latest apk add curl bash',
           "sed -i '1i #!/bin/bash' /codebuild/output/tmp/script.sh", //changes to correct bash shell to prevent error 2
           'head -n 1 /codebuild/output/tmp/script.sh',
           'echo "Image URI: ${DOCKER_ASSET_IMAGE_URI}"',
-          //'docker build ./CI/AWS/cdk/docker/', //last working build
-          'docker build -t local-tag ./CI/AWS/cdk/docker/',
-          'docker images',
-          "docker tag local-tag 475079312496.dkr.ecr.eu-west-1.amazonaws.com/digiroadautomation:$CODEBUILD_BUILD_NUMBER",
-          //"docker tag ${DOCKER_ASSET_IMAGE_URI} 475079312496.dkr.ecr.eu-west-1.amazonaws.com/digiroadautomation:$CODEBUILD_BUILD_NUMBER",
-          //"docker push ${DOCKER_ASSET_IMAGE_URI}",
-          //"docker tag digiroadautomation:latest 475079312496.dkr.ecr.eu-west-1.amazonaws.com/digiroadautomation:$CODEBUILD_BUILD_NUMBER",
-          `docker push 475079312496.dkr.ecr.eu-west-1.amazonaws.com/digiroadautomation:$CODEBUILD_BUILD_NUMBER`
+          'docker build -t digiroadautomation:latest ./CI/AWS/cdk/docker/',
+          'docker tag digiroadautomation:latest 475079312496.dkr.ecr.eu-west-1.amazonaws.com/digiroadautomation:$CODEBUILD_BUILD_NUMBER',
+          'docker tag digiroadautomation:latest 475079312496.dkr.ecr.eu-west-1.amazonaws.com/digiroadautomation:latest',
+          'docker push --all-tags 475079312496.dkr.ecr.eu-west-1.amazonaws.com/digiroadautomation'
           ],
           },
         },
@@ -71,7 +65,6 @@ export class TestAutomationCodeBuildStack extends cdk.Stack {
     });
     new cdk.CfnOutput(this, 'DRTestAutomationCfnOutputProject', {
       value: project.projectName,
-      //exportName: 'DigiroadTestAutomationProjectName',
     });
   }
 }
